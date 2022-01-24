da12afec919bf6b45268033568e2a093a24aa186
nicolasblumenroehr <86111342+nicolasblumenroehr@users.noreply.github.com>
2022-01-24 12:35:55 +0100

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,34 @@
+import json
+import os
+import glob
+from pyDicom import pyDicom
+from fillSchema import fillSchema
+from readSchema import readSchema
+
+jsonSchema = open("path/to/.json")
+draftDir=glob.glob("path/to/*draft.json")
+jsonSchema = json.load(jsonSchema)
+
+readSchema=readSchema()
+readSchema.schemaValidator(jsonSchema, draftDir)
+schema=readSchema.searchObject(jsonSchema)
+studyName="DicomTestStudy"
+studyLocation="URL"
+
+allDicomSeries=[]
+for i in os.listdir(studyLocation+studyName):
+    f = os.path.join(studyLocation+studyName, i)
+    seriesName=i
+    seriesLocation=studyLocation+studyName
+    imageFormatsList=["formats"] #one per image in a series
+    dicomSeries=pyDicom(f,seriesName, seriesLocation, imageFormatsList)
+    dicomSeries.seriesMetadata()
+    dicomSeries.studyMetadata(studyName, studyLocation)
+    allDicomSeries.append(dicomSeries)
+
+print("Schema structure",schema)
+fillSchema=fillSchema()
+filledSchema=fillSchema.fillObject(schema, list(schema.keys()), allDicomSeries)
+print("Mapping finished")
+with open('metadata.json', 'w') as f:
+    json.dump(filledSchema, f)
